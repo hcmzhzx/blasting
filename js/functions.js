288b872bc767bf8a60cf6d52a2a6489b343cb007
xiaohu <793538930@qq.com>
2018-08-14 10:56:05 +0800

    'b11'

--- js/functions.js
+++ js/functions.js
@@ -257,3 +257,18 @@ function searchToJson(paramstr, url) {
     }
     return JSON.parse('{' + newarr.join(',') + '}');
 }
+/**
+ * 节流函数 返回函数连续调用时
+ * @param method {function}  请求关联函数，实际应用需要调用的函数
+ * @param duration  {number} 延迟时间，单位毫秒
+ */
+const _throttle = (method, duration) => {
+    var begin = new Date();
+    return function () {
+        var current = new Date();
+        if (current - begin >= duration) {
+            method.apply(this, arguments);
+            begin = current;
+        }
+    }
+}
